1a6b902646855a115e1a0808d4e20ab2f4766ffe
Ekho852 <105564012+Ekho852@users.noreply.github.com>
2022-05-18 13:30:22 +0100

    Add files via upload

---
+++ Boolean_True_vs_False.py
@@ -0,0 +1,5 @@
+#Boolean Values which denote True or False Statements
+#True and False values are case sensitive and output represents binary True = 1, False = 0
+
+print(True > False)
+print(True < False)
