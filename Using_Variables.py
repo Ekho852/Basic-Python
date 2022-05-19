55509096049060f453866a1a84e697e17b580826
Ekho852 <105564012+Ekho852@users.noreply.github.com>
2022-05-19 14:09:28 +0100

    Add files via upload

---
+++ Using_Variables.py
@@ -0,0 +1,20 @@
+# Using Variables
+# var assigns variable in order of command line
+
+var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(var, account_balance, client_name)
+print(var)
+print()
+
+var = var + 1
+print(var)
+print()
+
+var = 100
+print(var)
+print()
+
+var = 200 + 300
+print(var)
