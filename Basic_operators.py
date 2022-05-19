70f5655c0d486ea00291a8b78591e05ae6eb3a5d
Ekho852 <105564012+Ekho852@users.noreply.github.com>
2022-05-19 12:12:28 +0100

    Add files via upload

---
+++ Basic_operators.py
@@ -0,0 +1,28 @@
+#Basic operators ie Maths
+# "+"" symbol is addition
+print(1+1)
+
+# "-"symbol is subtraction
+print()
+print(2-1)
+
+
+# "*" symbol is multiplication
+print()
+print(2*2)
+
+# "/" symbol is division (note: single \ generates float ie fraction)
+print()
+print(7/2)
+
+# "//" symbol is division (note: double \ generates integer and typically rounds down)
+print()
+print(7//2)
+
+# "%" symbol is division which returns remainder (note: value returned is only value of remaining regardless of number of divisions)
+print()
+print(7%5)
+
+# "**" symbol is exponent ie 2^4 (note: if one of the arguments is a float then result = float)
+print()
+print(2**4)
