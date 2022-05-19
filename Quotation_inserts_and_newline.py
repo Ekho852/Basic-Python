84d90b09e7ae274d0affab339e5a97ed4a6f4047
Ekho852 <105564012+Ekho852@users.noreply.github.com>
2022-05-19 12:12:40 +0100

    Add files via upload

---
+++ Quotation_inserts_and_newline.py
@@ -0,0 +1,16 @@
+#Basic operators ie Maths
+# "+"" symbol is addition
+print(1+1)
+# "-"symbol is subtraction
+print(2-1)
+# "*" symbol is multiplication
+print(2*2)
+# "/" symbol is division (note:)
+print(5/2)
+# "//" symbol is division (note:)
+print(5//2)
+# "%" symbol is division (note:)
+print(1%2)
+# "**" symbol is exponent ie 2^4
+print(2**4)
+print(2^4)
