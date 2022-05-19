175879969294497358155f46082d55421390aada
Ekho852 <105564012+Ekho852@users.noreply.github.com>
2022-05-19 13:07:15 +0100

    Update Monty_python_sep_and_end_argument.py

--- Monty_python_sep_and_end_argument.py
+++ Monty_python_sep_and_end_argument.py
@@ -1,1 +1,8 @@
-print("My", "name", "is", "Monty", "Python.", sep="-")
+#Keyword argument "sep" ie separator and "end"
+#line 5 prints "My name is" which is then separated by "_" and connects line 5 starting with "*"
+#line 6 prints "Monthy Python" which is then separated by "*" and connects the next line starting with "*" and a space
+
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+#Any keyword arguments have to be put after the last positional argument
