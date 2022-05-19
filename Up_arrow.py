4a6d1f7c86125e1058cae7881acae22565dfd109
Ekho852 <105564012+Ekho852@users.noreply.github.com>
2022-05-19 12:51:44 +0100

    Update Up_arrow.py

--- Up_arrow.py
+++ Up_arrow.py
@@ -1,5 +1,3 @@
-#Original
-
 print("    *")
 print("   * *")
 print("  *   *")
@@ -8,19 +6,3 @@ print("***   ***")
 print("  *   *")
 print("  *   *")
 print("  *****")
-print()
-
-#Using only one print function
-
-print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
-print()
-
-#Duplicate using *2
-print("    *         "*2)
-print("   * *        "*2)
-print("  *   *       "*2)
-print(" *     *      "*2)
-print("***   ***     "*2)
-print("  *   *       "*2)
-print("  *   *       "*2)
-print("  *****       "*2)
