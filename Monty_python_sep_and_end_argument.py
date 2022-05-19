13415a0af7dc2ac70646a943a787ea4142d46fb2
Ekho852 <105564012+Ekho852@users.noreply.github.com>
2022-05-19 13:03:50 +0100

    Update Monty_python_sep_and_end_argument.py

--- Monty_python_sep_and_end_argument.py
+++ Monty_python_sep_and_end_argument.py
@@ -1,9 +1,1 @@
-#Keyword argument "sep" ie separator and "end"
-#line 4 prints "My name is" which is then separated by "_" and connects line 5 starting with "*"
-#line 5 prints "Monthy Python" which is then separated by "*" and connects the next line starting with "*" and a space
-
-print("Programming","Essentials","in", sep="***", end="...")
-print("Python")
-
-
-#Any keyword arguments have to be put after the last positional argument
+print("My", "name", "is", "Monty", "Python.", sep="-")
