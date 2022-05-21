77c67e972f4f44dad44de9ac071bb0b0de3cc30b
Ekho852 <105564012+Ekho852@users.noreply.github.com>
2022-05-21 15:41:26 +0100

    Add files via upload

---
+++ Name_Concatenation.py
@@ -0,0 +1,6 @@
+# Name Concatenation
+
+fnam = input("May I have your first name, please? ")
+lnam = input("May I have your last name, please? ")
+print("Thank you.")
+print("\nYour name is " + fnam + " " + lnam + ".")
