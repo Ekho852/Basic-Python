8bf38c8a1ef97a0842196fc20a3804396b16d80e
Ekho852 <105564012+Ekho852@users.noreply.github.com>
2022-05-21 15:29:02 +0100

    Add files via upload

---
+++ Input_function.py
@@ -0,0 +1,7 @@
+#Input Function
+
+anything = input("Tell me anything...")
+print("Hmm...", anything, "... Really?")
+
+# input command generates window in console for user to input more text
+# this is then "replied to" by the print function above
