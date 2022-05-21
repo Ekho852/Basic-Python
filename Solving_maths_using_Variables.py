f569a17427b79bc035a6a268b6423c18b9727344
Ekho852 <105564012+Ekho852@users.noreply.github.com>
2022-05-21 14:13:12 +0100

    Add files via upload

---
+++ Solving_maths_using_Variables.py
@@ -0,0 +1,27 @@
+# Solving Maths using Variables
+#c = √ (a^2 + b^2) Represented below
+
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+
+print()
+
+# Asigning variables to people ie strings + integers
+
+john = 3
+mary = 5
+adam = 6
+
+print(john, mary, adam, sep=',')
+print()
+
+total_apples = john + mary + adam
+print(total_apples)
+print()
+
+print(adam/mary)
+print()
+
+print("Total number of apples:", total_apples)
