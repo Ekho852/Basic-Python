7e51df05748bdda15f22242cf941f00460f509cd
Ekho852 <105564012+Ekho852@users.noreply.github.com>
2022-05-21 15:07:21 +0100

    Add files via upload

--- Float_operators.py
+++ Float_operators.py
@@ -5,17 +5,27 @@ x = 0
 x = float(x)
 y = 3*x**3 - 2*x**2 + 3*x - 1
 print("y =", y)
-
 print()
 
 x = 1  
 x = float(x)
 y = 3*x**3 - 2*x**2 + 3*x - 1
 print("y =", y)
-
 print()
 
 x = -1  
 x = float(x)
 y = 3*x**3 - 2*x**2 + 3*x - 1
 print("y =", y)
+print()
+
+#Example of problem below
+
+a = '1'
+b = "1"
+print(a + b)
+print()
+
+a = 1
+b = 1
+print(a + b)
