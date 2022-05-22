3c6319bac03a0242db0c0a85d89d05760e42a0ab
Ekho852 <105564012+Ekho852@users.noreply.github.com>
2022-05-22 14:40:34 +0100

    Delete Monty_python_end_argument_x2.py

--- Monty_python_end_argument_x2.py
+++
@@ -1,5 +0,0 @@
-#Keyword argument "end" ie at to end
-
-print("My", "name", "is", "Monty", "Python",end=" My name is Monty Python")
-
-#Any keyword arguments have to be put after the last positional argument
