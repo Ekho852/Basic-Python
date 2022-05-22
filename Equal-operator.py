0559f02cf180d7adcb94409900e3f6d39d258699
Ekho852 <105564012+Ekho852@users.noreply.github.com>
2022-05-22 14:42:17 +0100

    Add files via upload

---
+++ Equal-operator.py
@@ -0,0 +1,20 @@
+#Equal and Inequal operator
+
+var = 0  # Assigning 0 to var
+print(var == 0)
+
+print()
+
+var = 1  # Assigning 1 to var
+print(var == 0)
+
+print()
+
+var = 0  # Assigning 0 to var
+print(var != 0)
+
+print()
+
+var = 1  # Assigning 1 to var
+print(var != 0)
+
---
+++ Higher_or_lower_input.py
@@ -0,0 +1,4 @@
+#Higher or lower than input
+
+n = int(input("Enter a number: "))
+print(n >= 100)
