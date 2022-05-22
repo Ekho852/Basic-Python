0a4453264740b1631a154ed8fdd4939666f99a36
Ekho852 <105564012+Ekho852@users.noreply.github.com>
2022-05-22 14:40:09 +0100

    Delete Boolean_True_vs_False.py

--- Boolean_True_vs_False.py
+++
@@ -1,5 +0,0 @@
-#Boolean Values which denote True or False Statements
-#True and False values are case sensitive and output represents binary True = 1, False = 0
-
-print(True > False)
-print(True < False)
